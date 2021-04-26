d43aab8a3874fc8b4622a7293a3fcbea14087bba
JuresK <76731039+JuresK@users.noreply.github.com>
2021-04-26 22:01:30 +0300

    Update juresk.js

--- juresk.js
+++ juresk.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client()
 const mongoose = require('mongoose');
 const fs = require('fs');
-const ayarlar = require('./ayarlar.json');
+const settings = require('./settings.json');
 require('./util/eventLoader.js')(client)
 mongoose.connect(ayarlar.mongoURL,{useNewUrlParser: true , useUnifiedTopology: true}).then((result) => console.log('Mongo Bağlantısı Kuruldu.')).catch((err) => console.log(err))
 client.commands = new Discord.Collection()
@@ -17,4 +17,4 @@ fs.readdirSync('./commands').forEach(klasor => {
 })
 
 
-client.login(ayarlar.token).then(x => console.log(`Bot ${client.user.username} Adıyla Giriş Yaptı!`)).catch(err => console.log(`Giriş Yapamadı! Hata: ${err}`))
+client.login(settings.token).then(x => console.log(`Bot ${client.user.username} Adlı Bota Giriş Yaptı!`)).catch(err => console.log(`Bota Giriş Yapamadı! Hata: ${err}`))
