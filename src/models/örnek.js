e8238b889da2ae6a037d13e987e2147378c0fa9f
JuresK <76731039+JuresK@users.noreply.github.com>
2021-04-26 22:38:18 +0300

    Update örnek.js

--- src/models/örnek.js
+++ src/models/örnek.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const Schema = new mongoose.Schema({
-  /* BURAYA KENDİ NİTELİKLERİNİZ. */
+  /* BURAYA KENDİ NİTELİKLERİNİZ. by Champ*/
 })
 
-module.exports = mongoose.model('Schema', Schema)
+module.exports = mongoose.model('Schema', Schema
