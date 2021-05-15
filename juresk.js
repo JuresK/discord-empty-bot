89db2389a7d28582cb479330d0ed1106b339d29c
JuresK <76731039+JuresK@users.noreply.github.com>
2021-05-15 15:16:26 +0300

    Update juresk.js

--- juresk.js
+++ juresk.js
@@ -1,18 +1,11 @@
-const Discord = require('discord.js');
-const client = new Discord.Client()
-const mongoose = require('mongoose');
-const fs = require('fs');
-const settings = require('./settings.json');
-require('./util/eventLoader.js')(client)
-client.commands = new Discord.Collection()
+const { Client, Collection } = require("discord.js");
+const settings = require("./src/configs/settings.json");
+const client = (global.client = new Client({ fetchAllMembers: true }));
+client.commands = new Collection();
+client.cooldown = new Map();
+require("./src/handlers/mongoHandler")(client);
 
-fs.readdirSync('./commands').forEach(klasor => {
-  const komutklasor = fs.readdirSync(`./commands/${klasor}`).filter(x => x.endsWith('.js'))
-  for(const komutlar of komutklasor) {
-  let komutlarr = require(`./commands/${klasor}/${komutlar}`)
-  console.log(`Komut Yüklendi: ${komutlarr.name}`)
-    client.commands.set(komutlarr.name, komutlarr)
-  }
-})
-
-client.login(settings.token).then(x => console.log(`Bot ${client.user.username} Adıyla Giriş Yapıldı!`)).catch(err => console.log(`Bota Giriş Yapamadı! Hata: ${err}`))
+client
+  .login(settings.token)
+  .then(() => console.log("[BOT] Bot başarıyla başlandı!"))
+  .catch(() => console.log("[BOT] Bot bağlanamadı!"));
